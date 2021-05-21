e6425a4f61be2f402108267a0d4b94a7625ff920
Radensuaso <andrels9283@gmail.com>
2021-05-21 14:45:01 +0100

    exercise 5 complete

--- D5.js
+++ D5.js
@@ -77,8 +77,27 @@ console.log(boundary(20));
 Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
+drawHeader("exercise 5");
 
-/* WRITE YOUR CODE HERE */
+const strivify = function(string) {
+    let words = string.toLowerCase().replace(",", "").split(" ");
+    let newWords = [];
+    if (words[0] === "strive") {
+        words[0] = "Strive";
+        return words.join(" ");
+    } else {
+        newWords.push("Strive");
+            for (let i = 0; i < words.length; i++) {
+                newWords.push(words[i])
+                }
+            }
+        return newWords.join(" ");    
+    }
+    
+
+console.log(strivify("Strive, is very cool indeed!"));
+console.log(strivify("What's my name?"));
+console.log(strivify("To live a good life."));
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
